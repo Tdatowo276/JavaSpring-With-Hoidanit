241596848df80c034fdee84beacc3c2633074c96
Nguyen Tien Dat <dat0343332294@gmail.com>
2026-09-11 22:26:27 +0700

    dont Render HTML

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,41 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+//Model: MVC
+@Controller
+public class UserController {
+
+    // DI: Dependency Injection
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        return "eric.html";
+    }
+}
+
+// RestfullAPI
+// @RestController
+// public class UserController {
+// // DI: Dependency Injection
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("/")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
